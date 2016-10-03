c76ad66a0a3a4e11cea743db93564a99934588c6
Ryan Vannin <ryan@plastical.com>
2016-10-03 17:42:02 +0200

    tests updated

--- rating/src/__tests__/Rating-test.js
+++ rating/src/__tests__/Rating-test.js
@@ -8,101 +8,75 @@ import Rating from '../Rating';
 //jest.mock('isomorphic-fetch');
 
 describe('<Rating />',() => {
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Rating />, div);
-});
+  it('renders without crashing', () => {
+    // pure Jest
+    const div = document.createElement('div');
+    ReactDOM.render(<Rating />, div);
+  });
 
-it('renders without crashing (with Shallow)', () => {
-  shallow(<Rating />);
-});
+  it('renders without crashing (with Shallow)', () => {
+    // with enzyme
+    shallow(<Rating />);
+  });
 
-it('renders a class named ".Rating"', () => {
-  const wrapper = shallow(<Rating />);
-  expect(wrapper.is('.Rating')).toBe(true);
-});
+  it('renders an element with a class named ".Rating"', () => {
+    // with enzyme
+    const wrapper = shallow(<Rating />);
+    expect(wrapper.is('.Rating')).toBe(true);
+  });
 
-it('renders N spans as default', () => {
-  const div = TestUtils.renderIntoDocument(
-    <Rating />
-  );
-  const divNode = ReactDOM.findDOMNode(div);
-  //console.log(TestUtils.isElementOfType(<Rating />, Rating));
-  //console.log(TestUtils.isDOMComponent(divNode));
-  var span = TestUtils.scryRenderedDOMComponentsWithTag(div,'span');
-  //console.log(span);
-  expect(span.length).toBe(2);  
-});
+  it('renders N spans as default', () => {
+    // with TestUtils
+    const wrapper = TestUtils.renderIntoDocument(<Rating />);
+    const wrapperNode = ReactDOM.findDOMNode(wrapper);
+    var el = TestUtils.scryRenderedDOMComponentsWithTag(wrapper,'span');
+    expect(el.length).toBe(2);  
+  });
 
-it('adds a class when clicked', () => {
-  const div = TestUtils.renderIntoDocument(
-    <Rating />
-  );
-  var span = TestUtils.scryRenderedDOMComponentsWithTag(div,'span');
-  //expect(span.length).toBe(2);
-  var aspan = TestUtils.scryRenderedDOMComponentsWithClass(div,'Rate--active');
-  //console.log(aspan);  
-  expect(aspan.length).toBe(1); 
-  TestUtils.Simulate.click(span[0]);
-  var cspan = TestUtils.scryRenderedDOMComponentsWithClass(div,'Rate--active');
-  //console.log(cspan);
-  expect(cspan.length).toBe(1);  
-  expect(cspan.length).not.toBe(2);
+  it('adds a class when clicked', () => {
+    // with TestUtils
+    const wrapper = TestUtils.renderIntoDocument(<Rating />);
+    var el = TestUtils.scryRenderedDOMComponentsWithTag(wrapper,'span');
+    var el_active = TestUtils.scryRenderedDOMComponentsWithClass(wrapper,'Rate--active');
+    expect(el_active.length).toBe(1); 
+    TestUtils.Simulate.click(el[0]);
+    var el_active = TestUtils.scryRenderedDOMComponentsWithClass(wrapper,'Rate--active');
+    expect(el_active.length).toBe(1);  
+    expect(el_active.length).not.toBe(2);
+    TestUtils.Simulate.click(el[1]);
+    var el_active = TestUtils.scryRenderedDOMComponentsWithClass(wrapper,'Rate--active');
+    expect(el_active.length).toBe(2);
+  });
 
-  TestUtils.Simulate.click(span[1]);
-  var ccspan = TestUtils.scryRenderedDOMComponentsWithClass(div,'Rate--active');
-  //console.log(ccspan);
-  expect(ccspan.length).toBe(2); 
-  //expect(div.changeRate).toBeCalled();
-});
+  it('adds a class when hovered', () => {
+    // with TestUtils
+    const wrapper = TestUtils.renderIntoDocument(<Rating />);
+    var el = TestUtils.scryRenderedDOMComponentsWithTag(wrapper,'span');
+    var el_hover = TestUtils.scryRenderedDOMComponentsWithClass(wrapper,'Rate--onover');
+    expect(el_hover.length).toBe(0); 
+    TestUtils.Simulate.mouseOver(el[0]);
+    var el_hover = TestUtils.scryRenderedDOMComponentsWithClass(wrapper,'Rate--onover');
+    expect(el_hover.length).toBe(1);  
+    TestUtils.Simulate.mouseOver(el[1]);
+    var el_hover = TestUtils.scryRenderedDOMComponentsWithClass(wrapper,'Rate--onover');
+    expect(el_hover.length).toBe(2); 
+  });
 
-it('adds a class when hovered', () => {
-  const div = TestUtils.renderIntoDocument(
-    <Rating />
-  );
-  var span = TestUtils.scryRenderedDOMComponentsWithTag(div,'span');
-  //expect(span.length).toBe(2);  
-  var aspan = TestUtils.scryRenderedDOMComponentsWithClass(div,'Rate--onover');
-  expect(aspan.length).toBe(0); 
-  TestUtils.Simulate.mouseOver(span[0]);
-  var cspan = TestUtils.scryRenderedDOMComponentsWithClass(div,'Rate--onover');
-  expect(cspan.length).toBe(1);  
-  TestUtils.Simulate.mouseOver(span[1]);
-  var ccspan = TestUtils.scryRenderedDOMComponentsWithClass(div,'Rate--onover');
-  expect(ccspan.length).toBe(2); 
-});
+  it('returns proper response', () => {
+    // with enzyme
+    const wrapper = mount(<Rating />);
+    expect(wrapper.node.handleAction('hover', 2)).toBe(true);
+    expect(wrapper.node.handleAction('onOver', 2)).not.toBe(true);
+    //console.log(wrapper.find('span'));
+  })
 
-it('returns proper response', () => {
-  const div = mount(<Rating />);
-  //console.log(div);
-  expect(div.node.handleAction('hover', 2)).toBe(true);
-  expect(div.node.handleAction('onOver', 2)).not.toBe(true);
-  //console.log(wrRatinger.find('span'));
-})
-
-it('propagates correct state', () => {
-  const div = mount(<Rating />);
-  console.log(div.state().tempRate);
-  //console.log(div.props());
-  expect(div.node.handleAction('hover', 2)).toBe(true);
-  //expect(div.props().rate).toBe(2);
-  expect(div.state().rate).toBe(2);
-  expect(div.node.handleAction('clicked', 1)).toBe(true);
-  console.log(div.state().tempRate);
-  expect(div.state().rate).toBe(1);
-  //var span = div.find('span');
-  //console.log(span.at(0));
-  //console.log(div.state().rate);
-  //TestUtils.Simulate.mouseOver(span.at(1));
-  //expect(span.at(1).hasClass('Rate--onover')).toBe(true);
-  //console.log(wrRatinger.find('span'));
-})
-
-//it('expects qty prop to be 2 (with Shallow)', () => {
-  //var renderer = TestUtils.createRenderer();
-  //renderer.render(<Rating />);
-  //var subject = renderer.getRenderOutput();
-  //expect(subject.props.qty).toBe(2); // => true
-  //console.log(subject.props);
-//});
+  it('propagates correct state', () => {
+    // with enzyme
+    const wrapper = mount(<Rating />);
+    expect(wrapper.node.handleAction('hover', 2)).toBe(true);
+    expect(wrapper.state().rate).toBe(2);
+    expect(wrapper.node.handleAction('clicked', 1)).toBe(true);
+    expect(wrapper.state().rate).toBe(1);
+    //expect(wrapper.props().rate).toBe(2);
+  })
 });
